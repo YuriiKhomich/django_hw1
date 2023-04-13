f2f8ecd1de052ce68d619e2df5025cedb85a2b02
YuriiKhomich <123492828+YuriiKhomich@users.noreply.github.com>
2023-04-13 22:17:25 +0300

    fix bugs

--- django_hw1/urls.py
+++ django_hw1/urls.py
@@ -18,5 +18,15 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+<<<<<<< HEAD
     path('admin/', admin.site.urls),
 ]
+=======
+    path('', myapp.views.index, name='index'),
+    path('login/', myapp.views.login, name="login"),
+    path('register/', myapp.views.register, name='register'),
+    path('main/', myapp.views.main, name='main'),
+    path('blog/', myapp.views.blog_detail, name='blog_detail'),
+    path('create_blog', myapp.views.create, name='create_blog')
+    ]
+>>>>>>> f671637 (HW2)
---
+++ myapp/views.py
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+=======
+
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def main(request):
+    return render(request, 'main.html')
+
+
+def blog_detail(request, name=''):
+    return render(request, 'blog_detail.html')
+
+
+def create(request):
+    return render(request, 'create_blog.html')
+>>>>>>> f671637 (HW2)
